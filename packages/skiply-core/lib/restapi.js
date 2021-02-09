a6ac2e7294282abd04f87b3c224a37b53398ca99
Aman Tandon <aman.tandon3096@gmail.com>
2021-02-09 12:33:02 +0530

    feat: added documentation feature in restapi.js

--- packages/skiply-core/lib/restapi.js
+++ packages/skiply-core/lib/restapi.js
@@ -1,12 +1,25 @@
 var api = require('api-chain');
 var request = require('request');
 var winston = require('winston');
-
+/**
+ * @param {string} skiply the skiply oject present here
+*/
 module.exports = function(skiply){
+    /**
+     * 
+     * @param {string} url string that is provided here 
+     * @param {string} cb sting cb is provied 
+     */
       skiply.get = function(url, cb) {
           request(skiply.url + url, {
                   'auth': skiply.auth
               },
+              /**
+               * 
+               * @param {object} error the error object
+               * @param {*} response the response object when successful
+               * @param {*} body the body of the response
+               */
               function(error, response, body) {
                   if (error) if(error)winston.error(error);
                   winston.info('GET API URL:' + skiply.url + url);
@@ -14,7 +27,12 @@ module.exports = function(skiply){
                   if (cb) cb(error, body);
               });
       }
-
+      /**
+       * 
+       * @param { string } url string provided 
+       * @param {*} data data provided here
+       * @param {*} cb cb provided in place
+       */
       skiply.post = function(url, data, cb) {
         var options = {
             uri: skiply.url + url,
@@ -24,6 +42,12 @@ module.exports = function(skiply){
           request(options, {
                   'auth': skiply.auth
               },
+              /**
+               * 
+               * @param {*} error error object if any
+               * @param {*} response response object if any
+               * @param {*} body response body if any
+               */
               function(error, response, body) {
                   if (error) if(error) winston.error(error);
                   winston.info('POST API URL:' + skiply.url + url);
